fec1971e6273983db12f54ce4036622a5f72e65f
Calum <calum@mackenzie-chambers.co.uk>
2022-12-06 22:16:52 +0000

    day 1 solutions

--- src/solutions/day_01.rs
+++ src/solutions/day_01.rs
@@ -4,12 +4,53 @@ pub struct Day1;
 
 impl Solution for Day1 {
     fn part_a(&self, input: &str) -> String {
-        todo!("Implement part A.\n\nInput file: \n{input}\n\n")
+        let mut max = 0;
+        let mut current = 0;
+
+        for line in input.lines() {
+            if line.is_empty() {
+                match current > max {
+                    true => max = current,
+                    false => (),
+                }
+                current = 0;
+                continue;
+            }
+            current += line.parse::<i32>().unwrap();
+        }
+
+        match current > max {
+            true => current.to_string(),
+            false => max.to_string(),
+        }
     }
 
     fn part_b(&self, input: &str) -> String {
-        todo!("Implement part B.\n\nInput file: \n{input}\n\n")
+        let mut maximum = [0; 3];
+        let mut current = 0;
+
+        for line in input.lines() {
+            if line.is_empty() {
+                insert_into_array(&mut maximum, current);
+                current = 0;
+                continue;
+            }
+            current += line.parse::<i32>().unwrap();
+        }
+        insert_into_array(&mut maximum, current);
+        maximum.iter().sum::<i32>().to_string()
     }
+}
+
+fn insert_into_array(array: &mut [i32], value: i32) {
+    if value < array[array.len() - 1] {
+        return;
+    }
+    let index = array.iter().position(|&x| x < value).unwrap();
+    for i in (index..array.len() - 1).rev() {
+        array[i + 1] = array[i];
+    }
+    array[index] = value;
 }
 
 // -------------------------------------------------- //
@@ -24,7 +65,7 @@ mod tests {
         let input = load(1, Some(true));
         let result = Day1::part_a(&Day1, &input);
 
-        todo!("Implement test for part A.\n\nResult: {result}")
+        assert_eq!(result, "24000");
     }
 
     #[test]
@@ -32,6 +73,22 @@ mod tests {
         let input = load(1, Some(true));
         let result = Day1::part_b(&Day1, &input);
 
-        todo!("Implement test for part B.\n\nResult: {result}")
+        assert_eq!(result, "45000");
+    }
+
+    #[test]
+    fn insert_small_value_into_array() {
+        let mut input = [7, 6, 5, 3];
+        insert_into_array(&mut input, 1);
+
+        assert_eq!(input, [7, 6, 5, 3])
+    }
+
+    #[test]
+    fn insert_into_array_eq() {
+        let mut input = [7, 6, 5, 3];
+        insert_into_array(&mut input, 7);
+
+        assert_eq!(input, [7, 7, 6, 5])
     }
 }
