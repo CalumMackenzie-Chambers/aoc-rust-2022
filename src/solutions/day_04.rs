05736fe550862573f00ab0cd141c5d6885e7a884
Calum <calum@mackenzie-chambers.co.uk>
2022-12-08 22:59:14 +0000

    day 4 solutions

--- src/solutions/day_04.rs
+++ src/solutions/day_04.rs
@@ -1,14 +1,69 @@
+use std::cmp::Ordering;
+
 use crate::Solution;
 
 pub struct Day4;
 
 impl Solution for Day4 {
     fn part_a(&self, input: &str) -> String {
-        todo!("Implement part A.\n\nInput file: \n{input}\n\n")
+        input
+            .lines()
+            .filter_map(|element| {
+                let numbers = extract_numbers(element);
+                match is_contained(&numbers) {
+                    true => Some(numbers),
+                    false => None,
+                }
+            })
+            .count()
+            .to_string()
     }
 
     fn part_b(&self, input: &str) -> String {
-        todo!("Implement part B.\n\nInput file: \n{input}\n\n")
+        input
+            .lines()
+            .filter_map(|element| {
+                let numbers = extract_numbers(element);
+                match does_overlap(&numbers) {
+                    true => Some(numbers),
+                    false => None,
+                }
+            })
+            .count()
+            .to_string()
+    }
+}
+
+// function to extract numbers from a string containing any characters
+fn extract_numbers(input: &str) -> Vec<i32> {
+    input
+        .split(|c: char| !c.is_numeric())
+        .filter(|s| !s.is_empty())
+        .map(|s| s.parse().unwrap())
+        .collect()
+}
+
+fn is_contained(input: &Vec<i32>) -> bool {
+    if input.len() != 4 {
+        return false;
+    }
+
+    match input[0].cmp(&input[2]) {
+        Ordering::Less => input[3] <= input[1],
+        Ordering::Equal => true,
+        Ordering::Greater => input[1] <= input[3],
+    }
+}
+
+fn does_overlap(input: &Vec<i32>) -> bool {
+    if input.len() != 4 {
+        return false;
+    }
+
+    match input[0].cmp(&input[2]) {
+        Ordering::Less => input[1] >= input[2],
+        Ordering::Equal => true,
+        Ordering::Greater => input[3] >= input[0],
     }
 }
 
@@ -24,7 +79,7 @@ mod tests {
         let input = load(4, Some(true));
         let result = Day4::part_a(&Day4, &input);
 
-        todo!("Implement test for part A.\n\nResult: {result}")
+        assert_eq!(result, "2")
     }
 
     #[test]
@@ -32,6 +87,6 @@ mod tests {
         let input = load(4, Some(true));
         let result = Day4::part_b(&Day4, &input);
 
-        todo!("Implement test for part B.\n\nResult: {result}")
+        assert_eq!(result, "4")
     }
 }
